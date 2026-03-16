254b7a5efa0eed276ccfa1e08a5795019f64cb55
진채영 <devchaezerojj@gmail.com>
2026-03-16 17:41:31 +0900

    feat(client): FCM 환경 셋업 - Capacitor, Firebase, Push Plugin
    S14P21E205-366

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.e205.eyespeak',
+  appName: 'eyespeak',
+  webDir: 'dist'
+};
+
+export default config;
