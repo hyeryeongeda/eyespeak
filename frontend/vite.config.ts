edea34ef7ce298d107ced41034ebd025fbb1631b
오언서 <vkvnxn15@naver.com>
2026-03-17 12:07:03 +0900

    fix(infra): Nginx upstream 수정 및 Vite 외부 접근 허용
    S14P21E205-436

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_URL || '/',
+  server: {
+    allowedHosts: ['j14e205.p.ssafy.io'],
+  },
 })
