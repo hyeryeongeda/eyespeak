6aba3317efd026623cc76e7d062031df036ea053
진채영 <devchaezerojj@gmail.com>
2026-03-20 10:23:48 +0900

    fix(client): 보호자 앱 css 전체 개선 및 파비콘, 앱 아이콘 등록

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.e205.eyespeak',
   appName: 'eyespeak',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000, // 스플래시 화면을 2초(2000ms) 동안 유지
+      launchAutoHide: true,     // 시간이 지나면 자동으로 사라짐
+      backgroundColor: "#FEFEFE", // 배경색 (흰색)
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,       // 로딩 중 동그라미(스피너) 숨기기
+    },
+  },
 };
 
 export default config;
