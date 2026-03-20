ab647927264754ee40856243c2334ddd2454cad2
박준수 <kookyss121@naver.com>
2026-03-20 15:18:26 +0900

    fix(server): dev 서버 CORS origin 추가
    S14P21E205

--- backend/src/main/java/e205/eyespeak/global/config/CorsConfig.java
+++ backend/src/main/java/e205/eyespeak/global/config/CorsConfig.java
@@ -26,10 +26,8 @@ public class CorsConfig {
         // 허용할 출처(프론트엔드 주소)
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",         // 로컬 Docker 프론트
-                "http://localhost:5173"          // 로컬 Vite 프론트
-                // TODO: 도메인 추가하기
-                // "https://도메인.com",            // prod 프론트
-                // "https://dev.도메인.com"         // dev 프론트
+                "http://localhost:5173",         // 로컬 Vite 프론트
+                "https://j14e205.p.ssafy.io"    // dev/prod 서버
         ));
 
         // 허용할 HTTP 메서드
